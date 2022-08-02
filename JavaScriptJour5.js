ccb81b93e73beeeb5d10ed3f0459fdc0269ec72b
Ber30 <makoberobertrand@gmail.com>
2022-08-02 19:45:11 +0200

    créer une méthode pour mon objet

--- JavaScriptJour5.js
+++ JavaScriptJour5.js
@@ -1,10 +1,17 @@
 let Person = {
-    FamilyName: "Makobero",
-    Surname: "Bertrand",
-    FatherName: "NIYIBIGIRA Salvator",
-    MotherName: "NDIKUMWAMI Jeannette",
-    LieuDeNaissance: "MUYINGA",
-    DateDeNaissance: 1997,
-    EtatCivil: "Célibataire",
+    Izina: "Makobero",
+    Itazirano: "Bertrand",
+    Se: "NIYIBIGIRA Salvator",
+    Nyina: "NDIKUMWAMI Jeannette",
+    Aho_Yavukiye: "MUYINGA",
+    Itariki_y_amavuka: 1997,
+    Etat_Civil: "Célibataire",
+    Akazi_Akora: "Etudiant",
     CNI: 531/1806/3680,
-}
+    NomComplet: function(){return this.Izina + ' '+this.Itazirano;}
+};
+// exercice 1: Récuperer une proprieté d'un objet;
+console.log(Person.Itazirano);
+
+// Créer une méthode qui me retourne mon nom complet
+console.log(Person.NomComplet());
