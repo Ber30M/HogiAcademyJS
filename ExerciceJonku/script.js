b8d033a8bf194ba2e3e6e84473acb7cc73251233
Ber30 <makoberobertrand@gmail.com>
2022-08-05 19:55:32 +0200

    Ajout des éléments HTML

---
+++ ExerciceJonku/script.js
@@ -0,0 +1,7 @@
+
+function twongeremwoAkantu(){
+    const akoTwongeramwo = document.createElement("div")
+    akoTwongeramwo.id = 'umwibutsa'
+    const ahoIja = document.getElementById("mere")
+    ahoIja.appendChild(akoTwongeramwo)
+}
